fcf94422fbbc2d2da3aeb3542289653504213db2
Ctibor Brančík <ctibor@brancik.cz>
2018-07-31 18:10:44 +0200

    Fix build issues with GCC-6.4

--- amh_focuser.cpp
+++ amh_focuser.cpp
@@ -15,6 +15,8 @@
  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
  Boston, MA 02110-1301, USA.
 *******************************************************************************/
+#include <cstdlib>
+#include <cmath>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
